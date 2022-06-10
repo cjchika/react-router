98827f2cb11aa852ce77b9c9caeb88d52ff88dc7
Chijioke Chika <8pluscreative@gmail.com>
2022-06-10 01:17:16 -0400

    Dynamic url

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,14 @@
 const Products = () => {
-	return <h1>The Products Page</h1>
+	return (
+		<section>
+			<h1>The Products Page</h1>
+			<ul>
+				<li>House</li>
+				<li>Car</li>
+				<li>Agriculture</li>
+			</ul>
+		</section>
+	);
 };
 
 export default Products;
