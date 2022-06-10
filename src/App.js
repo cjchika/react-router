b545dc4b8d134364b71f13f27dfeb3c1f617ef69
Chijioke Chika <8pluscreative@gmail.com>
2022-06-10 02:02:33 -0400

    Practice Project

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
-import { Route, Switch, Redirect } from "react-router-dom";
-import Products from "./pages/Products";
-import Welcome from "./pages/Welcome";
-import MainHeader from "./components/MainHeader";
-import ProductDetail from "./pages/ProductDetail";
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path='/' exact>
-            <Redirect to='/welcome' />
-          </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-          <Route path="/products/:productId">
-            <ProductDetail />
-          </Route>
-        </Switch>
-      </main>
-    </div>
+    <Switch>
+      <Route path="/quotes">
+
+      </Route>
+      <Route path="/quotes/:quoteId">
+
+      </Route>
+      <Route path="/new-quote">
+
+      </Route>
+    </Switch>
   );
 }
 
