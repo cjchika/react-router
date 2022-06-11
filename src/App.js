83b5a716d13f65c30bc842b20b3e8852db233dd2
Chijioke Chika <8pluscreative@gmail.com>
2022-06-11 00:55:08 -0400

    Updating

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Switch, Redirect } from "react-router-dom";
-import Layout from "./components/layout/layout";
+import Layout from "./components/layout/Layout";
 
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
--- src/pages/AllQuotes.jsx
+++ src/pages/AllQuotes.jsx
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const SUB_QUOTES = [
+	{ id: 'q1', author: "Mandela", text: 'It always seems impossible until its done.' },
+	{ id: 'q2', author: "Azikiwe", text: 'No matter how old an individual may be, no matter if he is young or old, if he thinks in accordance with the times he is immortal' }
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes</h1>
+  return <QuoteList quotes={SUB_QUOTES}/>
 };
 
 export default AllQuotes;
