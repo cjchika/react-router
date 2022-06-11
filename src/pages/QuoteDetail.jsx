a423447c93914d84db8a6c2317bccfacd960b320
Chijioke Chika <8pluscreative@gmail.com>
2022-06-11 00:02:27 -0400

    Update

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,5 +1,21 @@
+import { Fragment } from "react";
+import { useParams } from "react-router-dom";
+import { Route } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
+
 const QuoteDetail = () => {
-	return <h1>Quote Detail</h1>
+	const params = useParams()
+
+	return (
+		<Fragment>
+			<h1>Quote Detail</h1>
+			<p>{params.quoteId}</p>
+				<Route path={`/quotes/${params.quoteId}/comments`}>
+					<Comments />
+				</Route>
+		</Fragment>
+	)
 };
 
 export default QuoteDetail;
