90a858bdec2cd9ee289b9402e3a186fb93aef481
Chijioke Chika <8pluscreative@gmail.com>
2022-06-11 18:49:18 -0400

    Updating

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -3,14 +3,25 @@ import { useParams } from "react-router-dom";
 import { Route } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const SUB_QUOTES = [
+	{ id: 'q1', author: "Mandela", text: 'It always seems impossible until its done.' },
+	{ id: 'q2', author: "Azikiwe", text: 'No matter how old an individual may be, no matter if he is young or old, if he thinks in accordance with the times he is immortal' }
+];
 
 const QuoteDetail = () => {
 	const params = useParams()
 
+	const quote = SUB_QUOTES.find(quote => quote.id === params.quoteId);
+
+	if (!quote) {
+		return <p>No quote found</p>
+	}
+
 	return (
 		<Fragment>
-			<h1>Quote Detail</h1>
-			<p>{params.quoteId}</p>
+			<HighlightedQuote text={quote.text} author={quote.author}/>
 				<Route path={`/quotes/${params.quoteId}/comments`}>
 					<Comments />
 				</Route>
