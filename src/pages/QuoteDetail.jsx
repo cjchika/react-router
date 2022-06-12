25fa9f91833e0cb486c9fda01026ccdd812b7cbe
Chijioke Chika <8pluscreative@gmail.com>
2022-06-12 15:16:25 -0400

    Firebase

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,36 +1,43 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Route, Link, useRouteMatch } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
-const SUB_QUOTES = [
-  {
-    id: "q1",
-    author: "Mandela",
-    text: "It always seems impossible until its done.",
-  },
-  {
-    id: "q2",
-    author: "Azikiwe",
-    text: "No matter how old an individual may be, no matter if he is young or old, if he thinks in accordance with the times he is immortal",
-  },
-];
 
 const QuoteDetail = () => {
 	const match = useRouteMatch();
   const params = useParams();
 
-  const quote = SUB_QUOTES.find((quote) => quote.id === params.quoteId);
+  const { quoteId } = params;
 
-  if (!quote) {
-    return <p>No quote found</p>;
+  const { sendRequest, status, data: loadedQuotes, error } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId)
+  }, [sendRequest, quoteId])
+
+  if(status === 'pending') {
+    return <div className="centered">
+      <LoadingSpinner />
+    </div>
+  }
+
+  if(error) {
+    return <p className="centered">{error}</p>
+  }
+
+  if (!loadedQuotes.text) {
+    return <p className="centered">No quote found</p>;
   }
 
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuotes.text} author={loadedQuotes.author} />
       <Route path={match.path} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>
