4aaf4698b5ae2cdc00088517bbbea2cbce2e8d13
Chijioke Chika <8pluscreative@gmail.com>
2022-06-12 08:50:48 -0400

    Nested Routes

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,32 +1,48 @@
 import { Fragment } from "react";
 import { useParams } from "react-router-dom";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const SUB_QUOTES = [
-	{ id: 'q1', author: "Mandela", text: 'It always seems impossible until its done.' },
-	{ id: 'q2', author: "Azikiwe", text: 'No matter how old an individual may be, no matter if he is young or old, if he thinks in accordance with the times he is immortal' }
+  {
+    id: "q1",
+    author: "Mandela",
+    text: "It always seems impossible until its done.",
+  },
+  {
+    id: "q2",
+    author: "Azikiwe",
+    text: "No matter how old an individual may be, no matter if he is young or old, if he thinks in accordance with the times he is immortal",
+  },
 ];
 
 const QuoteDetail = () => {
-	const params = useParams()
+  const params = useParams();
 
-	const quote = SUB_QUOTES.find(quote => quote.id === params.quoteId);
+  const quote = SUB_QUOTES.find((quote) => quote.id === params.quoteId);
 
-	if (!quote) {
-		return <p>No quote found</p>
-	}
+  if (!quote) {
+    return <p>No quote found</p>;
+  }
 
-	return (
-		<Fragment>
-			<HighlightedQuote text={quote.text} author={quote.author}/>
-				<Route path={`/quotes/${params.quoteId}/comments`}>
-					<Comments />
-				</Route>
-		</Fragment>
-	)
+  return (
+    <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            See Comments
+          </Link>
+        </div>
+      </Route>
+
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
 };
 
 export default QuoteDetail;
